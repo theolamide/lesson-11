6f230d4791dbb514f5095b224c8e81bd07c0f3da
theolamide <ajadidan@gmail.com>
2020-02-10 16:32:24 -0700

    created cart component and added them to cart dropdown on product click

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartACtionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
 }
 
 export default CartACtionTypes;
